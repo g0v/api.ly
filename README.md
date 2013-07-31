ba1d366e90f2678f2406de86c15deaad912ed51a
Chia-liang Kao <clkao@clkao.org>
2013-07-31 16:46:18 +0800

    better installation with latest pgrest cookbook

--- README.md
+++ README.md
@@ -3,6 +3,29 @@ api.ly
 
 api.ly.g0v.tw endpoint source and utility scripts
 
+## init
+
+    % npm i
+
+Bootstrap with the initial dump file:
+
+    % createdb ly
+    % curl https://dl.dropboxusercontent.com/u/30657009/ly/api.ly.bz2 | psql ly -f -
+
+## calendar
+
+    % DB=ly lsc populate-calendar.ls  --year 2013
+
+## meeting agenda and proceeding
+
+## bill details
+
+## bill and motion metadata
+
+## interpellation
+
+## fulltext from gazettes
+
 License
 =======
 MIT: http://g0v.mit-license.org
--- cookbooks/ly.g0v.tw/recipes/default.rb
+++ cookbooks/ly.g0v.tw/recipes/default.rb
@@ -9,6 +9,7 @@ git "/opt/ly/twlyparser" do
 end
 
 execute "install twlyparser" do
+  cwd "/opt/ly/twlyparser"
   action :nothing
   subscribes :run, resources(:git => "/opt/ly/twlyparser")
   command "npm i && sudo npm link"
@@ -22,12 +23,15 @@ git "/opt/ly/api.ly" do
 end
 
 execute "install api.ly" do
+  cwd "/opt/ly/api.ly"
   action :nothing
   subscribes :run, resources(:git => "/opt/ly/api.ly")
-  command "sudo npm link twlyparser && npm i"
+  command "sudo npm link twlyparser && npm i && npm run prepublish"
+  notifies :restart, "service[lyapi]", :immediately
 end
 
 runit_service "lyapi" do
   default_logger true
+  action :enable
 end
 
