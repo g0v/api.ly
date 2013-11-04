8098f1db1af121095a0fb2ffc95b98c30f951afb
Chia-liang Kao <clkao@clkao.org>
2013-11-04 10:17:08 -0800

    split stream-worker

--- cookbooks/ly.g0v.tw/recipes/stream.rb
+++ cookbooks/ly.g0v.tw/recipes/stream.rb
@@ -1,39 +1,5 @@
 include_recipe "nginx::source"
-
-package "pkg-config"
-package "yasm"
-package "libvorbis-dev"
-package "libvpx-dev"
-package "libx264-dev"
-package "libavcodec-extra-53"
-package "libmp3lame-dev"
-package "libfaac-dev"
-
-git "/opt/ffmpeg" do
-  repository "git://github.com/FFmpeg/FFmpeg"
-  reference "n1.0.8"
-  action :sync
-end
-
-execute "install ffmpeg" do
-  cwd "/opt/ffmpeg"
-  action :nothing
-  subscribes :run, resources(:git => "/opt/ffmpeg"), :immediately
-  command "./configure --enable-libvpx --enable-libvorbis --enable-libx264 --enable-gpl --enable-nonfree --enable-libmp3lame --enable-libfaac && make"
-end
-
-git "/opt/msdl" do
-  repository "git://github.com/clkao/msdl"
-  reference "master"
-  action :sync
-end
-
-execute "install msdl" do
-  cwd "/opt/msdl"
-  action :nothing
-  subscribes :run, resources(:git => "/opt/msdl"), :immediately
-  command "./configure && make"
-end
+include_recipe "ly.g0v.tw::stream-worker"
 
 directory "/var/run/hls" do
   action :create
@@ -72,17 +38,4 @@ end
 runit_service "ffserver" do
   default_logger true
   action [:enable, :start]
-end
-
-node[:ly][:channels].each do |ch|
-  runit_service "msdl-#{ch.channel}" do
-    run_template_name "msdl"
-    default_logger true
-    action [:create]
-    options ({
-      :chid => ch.chid,
-      :channel => ch.channel,
-      :rtmp_server => 'rtmp://localhost:1935/hls'
-    })
-  end
 end
